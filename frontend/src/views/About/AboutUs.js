5f51cad713524b824589d06147abcddced0384d8
rafapm1999 <rportillom99.code@gmail.com>
2023-07-30 15:11:49 +0200

    Starting the Contact Us site

--- frontend/src/views/About/AboutUs.js
+++ frontend/src/views/About/AboutUs.js
@@ -8,7 +8,17 @@ function AboutUs() {
           <div className={classes.title}>
             <h1>About us</h1>
           </div>
-          <div></div>
+          <div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
+              dolore neque autem nam dignissimos ut tempore temporibus
+              laboriosam. Beatae nisi aspernatur consectetur. Aliquam voluptate
+              ut laborum deserunt eveniet consequuntur blanditiis ex illo
+              ratione quo dolor architecto, quis placeat sapiente quia, iusto
+              non numquam molestiae inventore animi magnam suscipit obcaecati
+              unde.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/views/Contact/ContactUs.js
+++ frontend/src/views/Contact/ContactUs.js
@@ -1,9 +1,68 @@
-import classes from './ContactUs.module.css';
+import classes from "./ContactUs.module.css";
+import { useRef } from "react";
 
 function ContactUs() {
+  const nameRef = useRef("");
+  const lastNameRef = useRef("");
+  const emailRef = useRef("");
+  const userCommentRef = useRef("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const contactData = {
+      name: nameRef.current.value,
+      lastName: lastNameRef.current.value,
+      email: emailRef.current.value,
+      userComment: userCommentRef.current.value,
+    };
+  };
   return (
-    <div>
+    <div className="contactUs-root">
+      <div className={classes.title}>
         <h1>Contact Us</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={classes["form-info"]}>
+          <input
+            ref={nameRef}
+            type="text"
+            name="name"
+            id="userName"
+            placeholder="Name"
+          />
+        </div>
+        <div className={classes["form-info"]}>
+          <input
+            ref={lastNameRef}
+            type="text"
+            name="lastName"
+            id="userLastName"
+            placeholder="Lastname"
+          />
+        </div>
+        <div className={classes["form-info"]}>
+          <input
+          ref={emailRef}
+          type="email"
+          name="email"
+          id="userEmail"
+          placeholder="Email"
+          />
+        </div>
+        <div className={classes["form-info"]}>
+          <textarea 
+            ref={userCommentRef}
+            name="userComment"
+            id="userCommentText"
+            placeholder="What do you tell to us?"
+            maxlength="50"
+          ></textarea>
+        </div>
+        <div className={`${classes["form-info"]} ${classes["submit-button"]}`}>
+          <button type="submit">
+            <span>Send</span>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
